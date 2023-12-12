63c6f1536619a002a10c52757c406535c91ac9ed
Consultant <samirsamado5@gmail.com>
2023-12-12 12:12:09 -0500

    add ItemModel & admin

--- trackApp/models.py
+++ trackApp/models.py
@@ -2,20 +2,26 @@ from django.db import models
 from django.utils import timezone
 
 
+class ItemModel(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    quantity = models.IntegerField(null=False, blank=False, default=1)
+    price = models.FloatField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
+    item = models.ForeignKey(ItemModel, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100, null=False, blank=False)
     date_of_purchase = models.DateTimeField(default=timezone.now)
     price = models.DecimalField(max_digits=1000, decimal_places=2)
 
     def calculate_total_amount(self):
-        # Retrieve all items related to the current recipe
-        items = Recipe.objects.filter(name=self.name)
-
         # Calculate the total amount based on the prices of items
-        total_amount = sum(item.price for item in items)
-
+        total_amount = self.item.price * self.item.quantity
         return total_amount
 
     def __str__(self):
         return self.name
-
