7e8f2a3370f9c0720a07fc6143c972ae265d8ff9
Consultant <samirsamado5@gmail.com>
2023-12-11 21:08:03 -0500

    create Receipt model

--- trackApp/models.py
+++ trackApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    date_of_purchase = models.DateTimeField(default=timezone.now)
+    price = models.DecimalField(max_digits=1000, decimal_places=2)
+
+    def calculate_total_amount(self):
+        # Retrieve all items related to the current recipe
+        items = Recipe.objects.filter(name=self.name)
+
+        # Calculate the total amount based on the prices of items
+        total_amount = sum(item.price for item in items)
+
+        return total_amount
+
+    def __str__(self):
+        return self.name
+
