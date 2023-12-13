0359fe7ed88f7937fcd28bb50b18b1293816bbf6
Consultant <samirsamado5@gmail.com>
2023-12-13 15:00:13 -0500

    django datePicker

--- trackApp/forms.py
+++ trackApp/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+
 from .models import Recipe
+from django.forms import DateInput
 
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['store_name', 'date_of_purchase', 'item', 'quantity', 'price', 'status']
+        widgets = {
+            'date_of_purchase': DateInput(attrs={'type': 'date'}),
+        }
