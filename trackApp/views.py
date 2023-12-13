09912a03328ccd85ed1256296c38fcb89fdddb59
Consultant <samirsamado5@gmail.com>
2023-12-12 21:46:49 -0500

    track item if exists

--- trackApp/views.py
+++ trackApp/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from trackApp.models import Recipe
@@ -26,3 +27,25 @@ def details(request, id):
         'recipe': recipe,
     }
     return render(request, 'trackApp/details.html', context)
+
+
+def track_item(request):
+    search_item = request.GET.get('search')
+    items_found = None
+
+    if search_item != "":
+        items_found = Recipe.objects.filter(Q(id__icontains=search_item))
+        if items_found.exists():
+            # TODO: add logger and alerts
+            print("Item exists in the database!")
+        else:
+            print("Item doesn't exist in the database.")
+    else:
+        print("No search item provided")
+
+    # recipe = get_object_or_404(Recipe, id=id)
+    context = {
+        "items_found": items_found,
+    }
+
+    return render(request, 'trackApp/track_item.html', context)
