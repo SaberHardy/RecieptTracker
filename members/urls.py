8d9d02da2ad91dbdf5c0a1ce0747adafa9fcdecf
Consultant <samirsamado5@gmail.com>
2023-12-13 21:42:50 -0500

    fix login error

--- members/urls.py
+++ members/urls.py
@@ -5,4 +5,5 @@ from members import views
 urlpatterns = [
     path('login_user/', views.login_user, name='login_user'),
     path('register_user/', views.register_user, name='register_user'),
+    path('logout_user/', views.logout_user, name='logout_user'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -5,22 +6,21 @@ from django.contrib import messages
 
 def login_user(request):
     if request.method == 'POST':
-        user_name = request.POST['username']
+        username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(user_name=user_name, password=password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
             print("Logged in successfully")
-            messages.error(request, 'You are now logged in as %s' % user_name)
+            messages.success(request, 'You are now logged in as %s' % username)
             return redirect('index')
         else:
             print("Invalid username or password")
             messages.error(request, 'Invalid username or password')
             return redirect('login')
-    else:
-        print("Invalid username or password second")
-        return render(request, 'registration/login.html')
+    print("Redirected to login again!!")
+    return render(request, 'registration/login.html')
 
 
 def register_user(request):
@@ -32,5 +32,9 @@ def register_user(request):
         user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
         user.save()
 
-
     return render(request, 'registration/register.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
